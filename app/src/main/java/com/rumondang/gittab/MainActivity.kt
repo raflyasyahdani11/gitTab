afdf35b7aabfb44c97fc2731f6edaa071cbc884f
rumondang <rumondang18ti@mahasiswa.pcr.ac.id>
2020-12-06 16:49:22 +0700

    halaman sent

--- app/src/main/java/com/rumondang/gittab/MainActivity.kt
+++ app/src/main/java/com/rumondang/gittab/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
 
+        viewPagerAdapter.tambahFragment(SentFragment(), "Sent")g
         viewPager.adapter = viewPagerAdapter
         tabLayout.setupWithViewPager(viewPager)
     }
