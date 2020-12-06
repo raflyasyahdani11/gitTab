3ebfa1690d147e3eb0f7c7856f5b95515ce93893
rumondang <rumondang18ti@mahasiswa.pcr.ac.id>
2020-12-06 16:38:46 +0700

    komit pertama

---
+++ app/src/main/java/com/rumondang/gittab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rumondang.gittab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
